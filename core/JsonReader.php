6f96cd1de96322a3fadd34cc4e210cf8be85d3fb
SerzhKorochan <serzhkorochan@gmail.com>
2022-08-17 10:12:51 +0300

    added node's info get functions

--- core/JsonReader.php
+++ core/JsonReader.php
@@ -2,13 +2,27 @@
     class JsonReader {
         public $data;
 
-        function __construct($fileName) {
+        function __construct($fileName){
             if (!file_exists($fileName)) {
                 throw new Exception('File does not exist!');
             }
 
             $fileContent = file_get_contents($fileName);
             $this->data = json_decode($fileContent, $assoc = true);
+        }
+
+        public function getDataByNodeName($nodeName){
+            foreach ($this->data['server_data'] as $node) {
+                if (array_key_exists($nodeName, $node)){
+                    return $node[$nodeName];
+                }
+            }
+            
+            throw new Exception('Node <' . $nodeName . '> does not exist!');
+        }
+
+        public function getAllData(){
+            return $this->data;
         }
     }
 ?>
--- index.php
+++ index.php
@@ -2,3 +2,6 @@
     require 'core/JsonReader.php';
 
     $obj = new JsonReader('data/sdata.json');
+
+    var_dump($obj->getAllData());
+    var_dump($obj->getDataByNodeName('DP90'));
